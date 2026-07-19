7096f59b40895ec64e64fe59946bdfc2399efc91
miqdadabdillah <mqddabdillah@gmail.com>
2026-07-19 07:43:40 +0700

    feat: implement SEO metadata, structured data, and index pages

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Just Sharing by MA';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Blog tips dan tutorial teknologi: printer, software, dan solusi IT sehari-hari. Temukan panduan praktis yang mudah dipahami.';
